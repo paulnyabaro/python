93a06e784aab0f1be647b393bcd29953ffd6af98
paulnyabaro <paulnyabaro1@gmail.com>
2024-04-09 07:06:21 +0300

    Using global variable

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python learning notes
+
+use global keyword to create a global variable from a function
--- a.py
+++ a.py
@@ -2,4 +2,13 @@
 fruits = ['Apple', 'Banana', 'Orange']
 x, y, z = fruits
 
+print(x, y, z) # Comma supports different data types while + support same type
+
+def fun():
+    global j
+    j = 'This are'
+
+fun()
+
 print(x)
+print(j)
